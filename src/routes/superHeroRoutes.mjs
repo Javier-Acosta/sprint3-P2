72aff18d9a434e2b095a480022a19c0794e8bb80
Javier_Acosta <jacosta@catamarca.gov.ar>
2024-12-05 08:23:11 -0300

    Actualizado

--- src/routes/superHeroRoutes.mjs
+++ src/routes/superHeroRoutes.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { obtenerSuperheroePorIdController, obtenerTodosLosSuperheroesController, buscarSuperheroesPorAtributoController, obtenerSuperheroesMayoresDe30Controller, agregarSuperheroeController, actualizarSuperheroeController, eliminarSuperheroePorIdController, eliminarSuperheroePorNombreController } from '../controllers/superheroesController.mjs';
 import { handleVAlidationErrors } from '../middlewares/errorMiddleware.mjs';
+import { heroeValidator } from '../validators/heroeValidator.mjs';
 
 const router = express.Router();
 
@@ -10,9 +11,9 @@ router.get('/heroes/:id', obtenerSuperheroePorIdController);
 router.get('/heroes/buscar/:atributo/:valor', buscarSuperheroesPorAtributoController);
 router.get('/heroes/mayores-30', obtenerSuperheroesMayoresDe30Controller);
 
-router.post('/heroes/heroe-nuevo', handleVAlidationErrors, agregarSuperheroeController);
-router.put('/heroes/:id', handleVAlidationErrors, actualizarSuperheroeController);
-router.delete('/heroes/:id', handleVAlidationErrors, eliminarSuperheroePorIdController);
-router.delete('/heroes/nombre/:nombre', handleVAlidationErrors, eliminarSuperheroePorNombreController);
+router.post('/heroes/heroe-nuevo',heroeValidator(), handleVAlidationErrors, agregarSuperheroeController);
+router.put('/heroes/:id', heroeValidator(),handleVAlidationErrors, actualizarSuperheroeController);
+router.delete('/heroes/:id', eliminarSuperheroePorIdController);
+router.delete('/heroes/nombre/:nombre',  eliminarSuperheroePorNombreController);
 
 export default router;
